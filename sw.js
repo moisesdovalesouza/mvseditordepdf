ae274b76e10a8510160e5a41a52a8fbd41c09b7f
moisesdovalesouza <moisesdovalesouza@gmail.com>
2026-09-21 12:03:09 -0300

    v46: limpa cache legado e atualiza PWA

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = "mvs-editor-de-pdf-v45-paper-safe";
+const CACHE = "mvs-editor-de-pdf-v46-stable";
 const ASSETS = [
-  "./","./?pwa=v45","./index.html","./style.css","./theme-v38.css","./fix-v40.css","./fix-v41.css","./fix-v42.css","./fix-v43.css","./fix-v44.css","./fix-v45.css","./app.js","./manifest.webmanifest","./browserconfig.xml",
+  "./","./?pwa=v46","./index.html","./style.css","./theme-v38.css","./app.js","./manifest.webmanifest","./browserconfig.xml",
   "./icons/brand-ui-light-v37.png","./icons/brand-ui-dark-v37.png",
   "./icons/favicon-v37-64.png",
   "./icons/mvs-app-v37-192.png","./icons/mvs-app-v37-512.png","./icons/mvs-app-v37-maskable-512.png",
